58bc065434851eb52374e566a743edfd5575b629
unknown <guillermo.sequeira@wolox.com.ar>
2019-11-09 20:10:10 -0300

    mejora de la pantalla principal

--- index.py
+++ index.py
@@ -13,27 +13,38 @@ class File:
     def __init__(self, window):  # constructor
         self.wind = window  # almaceno window en 'self.wind' para ponerle titulo
         self.wind.title("Cryptography Application")
-        self.wind.geometry("500x400")
+        self.wind.geometry("600x500")
 
-        frame = LabelFrame(
-            self.wind, text="Ingrese el archivo que desea Encriptar/Desencriptar"
-        )
+        frame = Frame(self.wind, width=100, height=100, relief="solid", bd=1)
         # le ponemos titulo al recuadro
-        frame.grid(row=0, column=0, columnspan=3, pady=20, padx=80)
+        # frame.grid(row=0, column=0, columnspan=3, pady=20, padx=80)
         # manera en la que vamos a pocisionar los elementos mediante grid
 
-        self.text = Entry(frame, width=60)
+        frame.place(x=155, y=80)
+
+        text = Label(
+            frame, text="Ingrese el archivo que desea Encriptar/Desencriptar"
+        ).grid(row=1, column=0, pady=30)
+
+        self.text = Entry(frame, width=30)
+        self.text.place(x=10, y=70, width=250, height=30)
+        self.text.configure(state="readonly")
 
         self.botonAbrir = Button(
             frame, text="Seleccionar archivo", command=lambda: abrir_archivo(self.text)
         )
         self.botonAbrir.grid(row=3, column=0, pady=30)
+
         self.radioButtonEncrypt = Radiobutton(
-            frame, text="Encriptar", padx=20, value=1, variable=selec
+            frame, text="Encriptar      ", value=1, variable=selec
         ).grid(row=4, column=0)
+
         self.radioButtonEncrypt = Radiobutton(
-            frame, text="Desencriptar", padx=30, value=2, variable=selec
+            frame, text="Desencriptar", padx=90, value=2, variable=selec
         ).grid(row=5, column=0)
+
+        self.botonComenzar = Button(frame, text="Comenzar", padx=30)
+        self.botonComenzar.grid(row=6, column=0, pady=30)
 
 
 def abrir_archivo(text):
@@ -47,7 +58,6 @@ def abrir_archivo(text):
     text.delete(0, END)
     text.insert(0, abs_path)
     text.configure(state="readonly")
-    text.grid(row=0, column=0)
 
 
 # funcion main la cual ejecutara nuestra ventana
